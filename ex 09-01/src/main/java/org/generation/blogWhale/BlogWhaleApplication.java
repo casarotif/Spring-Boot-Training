0d7159449937f75fa53896605da233bd278f510d
Fabio Ferreira Casaroti <fabiocasaroti@hotmail.com>
2020-09-01 17:45:01 -0300

    Exercícios 01/09

---
+++ ex 09-01/src/main/java/org/generation/blogWhale/BlogWhaleApplication.java
@@ -0,0 +1,13 @@
+package org.generation.blogWhale;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BlogWhaleApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BlogWhaleApplication.class, args);
+	}
+
+}
